7cd62ffe16379ce48571b8222c89dd2ad613f6ef
chenyunli <m18301662790@gmail.com>
2021-01-08 17:42:11 +0800

    Importing packages from remote modules

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/user/hello1
 
 go 1.15
+
+require github.com/google/go-cmp v0.5.4
--- hello1.go
+++ hello1.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"example.com/user/hello1/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
